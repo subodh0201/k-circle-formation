2cddc65300a57cc2b4c35141268caf65e69db563
Subodh Singh <singh.01.subodh@gmail.com>
2023-01-25 20:33:38 +0530

    Add Path

---
+++ src/main/java/sbc/grid/robot/Path.java
@@ -0,0 +1,45 @@
+package sbc.grid.robot;
+
+import sbc.grid.Point;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Path implements Iterable<Direction> {
+    private final Point start;
+    private final List<Direction> directionList;
+
+    public Path(Point start, List<Direction> directionList) {
+        this.start = start;
+        this.directionList = Collections.unmodifiableList(directionList);
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public List<Direction> getDirectionList() {
+        return directionList;
+    }
+
+    @Override
+    public Iterator<Direction> iterator() {
+        return new PathIterator();
+    }
+
+    private class PathIterator implements Iterator<Direction> {
+
+        int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < directionList.size();
+        }
+
+        @Override
+        public Direction next() {
+            return directionList.get(index++);
+        }
+    }
+}

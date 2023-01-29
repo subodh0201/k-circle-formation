3b527b434c3cb434880186ba143e737557bb05b6
Subodh Singh <singh.01.subodh@gmail.com>
2023-01-29 17:23:47 +0530

    Add Background

--- src/main/java/sbc/gui/App.java
+++ src/main/java/sbc/gui/App.java
@@ -1,15 +1,29 @@
 package sbc.gui;
 
+import sbc.kcf.KcfSimulation;
+import sbc.kcf.KcfSimulationRenderer;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class App extends JFrame {
+
+    private final GridScene gridScene;
 
     public App(String title) {
         super(title);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(512, 512));
         this.setBackground(Color.CYAN);
+
+        this.gridScene = new GridScene();
+        this.add(gridScene);
         this.setVisible(true);
+    }
+
+    public void addSimulation(KcfSimulation  kcfSimulation) {
+        this.gridScene.addEntity(new Background());
+        this.gridScene.addEntity(new KcfSimulationRenderer(kcfSimulation));
+        this.gridScene.start();
     }
 }
---
+++ src/main/java/sbc/gui/Background.java
@@ -0,0 +1,50 @@
+package sbc.gui;
+
+import java.awt.*;
+
+public class Background implements GridEntity {
+    public final Color oddTiles;
+    public final Color evenTiles;
+    public final Color axisBorders;
+
+    public Background() {
+        this(Color.WHITE, new Color(245, 245, 245), Color.BLUE);
+    }
+
+    public Background(Color oddTiles, Color evenTiles, Color axisBorders) {
+        this.oddTiles = oddTiles;
+        this.evenTiles = evenTiles;
+        this.axisBorders = axisBorders;
+    }
+
+    @Override
+    public boolean update() {
+        // nothing to update
+        return false;
+    }
+
+    @Override
+    public void render(Graphics2D g, GridViewPort v) {
+        for (int tileY = 0; tileY < v.tileCountY(); tileY++) {
+            for (int tileX = 0; tileX < v.tileCountX(); tileX++) {
+                boolean even = (v.tileToGridX(tileX) + v.tileToGridY(tileY)) % 2 == 0;
+                g.setColor(even ? evenTiles : oddTiles);
+                g.fillRect(v.tileToScreenX(tileX), v.tileToScreenY(tileY), v.tileSize(), v.tileSize());
+            }
+        }
+
+        g.setColor(axisBorders);
+        // y-axis
+        if (v.isVisibleGridX(0)) {
+            for (int tileY = 0; tileY < v.tileCountY(); tileY++) {
+                g.drawRect(v.gridToScreenX(0), v.tileToScreenY(tileY), v.tileSize(), v.tileSize());
+            }
+        }
+        // x-axis
+        if (v.isVisibleGridY(0)) {
+            for (int tileX = 0; tileX < v.tileCountX(); tileX++) {
+                g.drawRect(v.tileToScreenX(tileX), v.gridToScreenY(0), v.tileSize(), v.tileSize());
+            }
+        }
+    }
+}
